ec07dc7c3c8072c3b89f73add57d6521f0027a0d
Craig Sharkie <craig.sharkie@gmail.com>
2017-03-29 05:29:56 +1100

    Adding IFRAME generating scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,49 @@
-"use strict;"
+'use strict';
 
 (function(){
     const SVNLV = {
-      display: function (title, data, members) {
+        SOURCE: "svnlv",
+        TARGET: null,
+        STYLE: `
+            .svnlv {
+                height: 0;
+                overflow: hidden;
+                padding-bottom: 56.25%; /* 75% */
+                position: relative;
+            }
+            .svnlv iframe {
+                position: absolute;
+                top:0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+            }
+        `,
+        create: function () {
+            const stage = document.createElement("div");
+            const iframe = document.createElement("iframe");
+            const style = document.createElement("style");
+            const head = document.querySelector("head");
+            const target = document.querySelector("#" + SVNLV.TARGET);
 
-      },
+            style.type = "text/css";
+            stage.classList.add("svnlv");
+            iframe.src = "https://7live.com.au/";
 
-      init: function () {
-      }
+            style.appendChild(document.createTextNode(SVNLV.STYLE));
+            head.appendChild(style);
+            stage.appendChild(iframe);
+            target.appendChild(stage);
+        },
+        getTarget: function () {
+            const source = document.querySelector("script#" + SVNLV.SOURCE);
+            const target = source.dataset.target;
+            SVNLV.TARGET = target;
+        },
+        init: function () {
+            this.getTarget();
+            this.create();
+        }
     };
 
     window.SVNLV = SVNLV;
